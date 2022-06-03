11ac231cb57ede50debfea2c31a1686a6404f220
NJOKU OKECHUKWU VALENTINE <bonsoirval@gmail.com>
2022-06-03 11:55:22 +0100

    Test api request working

--- api.py
+++ api.py
@@ -22,24 +22,30 @@ import pickle
 import json
 import numpy as np
 from model import load_model, make_prediction
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 # Application definition
 app = Flask(__name__)
 
 # Load our model into memory.
 # Please update this path to reflect your own trained model.
-static_model = load_model(
-    path_to_model='assets/trained-models/load_shortfall_simple_lm_regression.pkl')
+static_model = load_model(path_to_model='assets/trained-models/load_shortfall_simple_lm_regression.pkl')
 
 print ('-'*40)
 print ('Model successfully loaded')
 print ('-'*40)
 
-""" You may use this section (above the app routing function) of the python script to implement 
+""" You may use this section (above the app routing function) of the python script to implement
     any auxiliary functions required to process your model's artifacts.
 """
 
+# @app.route("/api_v0.1", methods=['POST'])
+# def predict():
+#     return 'hello'
+
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 # Define the API's interface.
 # Here the 'model_prediction()' function will be called when a POST request
@@ -54,6 +60,7 @@ def model_prediction():
     output = make_prediction(data, static_model)
     # We finally package this prediction as a JSON object to deliver a valid
     # response with our API.
+    # return render_template('result.html', output=output) # "Working"
     return jsonify(output)
 
 # Configure Server Startup properties.
